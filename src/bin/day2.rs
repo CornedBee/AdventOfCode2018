8d2ee499888262bf50e4f4adca6ad0c3e0475c6a
Sebastian Redl <wasti.redl@gmx.net>
2018-12-02 22:20:17 +0100

    Implement day 2 part 1.

---
+++ src/bin/day2.rs
@@ -0,0 +1,7 @@
+const INPUT: &str = include_str!("../../inputs/day2");
+
+fn main() {
+    let results = aoc2018::day2(INPUT);
+    println!("{}", results.0);
+    println!("{}", results.1);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::str::FromStr;
 
@@ -16,4 +17,33 @@ pub fn day1(input: &str) -> Result<(i32, i32), std::num::ParseIntError> {
     }
 
     unreachable!("cycle() is infinite");
+}
+
+pub fn day2(input: &str) -> (i32, i32) {
+    let checksum_parts = input.lines().map(checksum_increments)
+        .fold((0, 0), |(x1, x2), (y1, y2)| (x1 + y1, x2 + y2));
+
+    (checksum_parts.0 * checksum_parts.1, 0)
+}
+
+fn checksum_increments(word: &str) -> (i32, i32) {
+    let counts = letter_counts(word);
+    let mut result = (0, 0);
+    for (_, v) in counts {
+        match v {
+            2 => result.0 = 1,
+            3 => result.1 = 1,
+            _ => ()
+        }
+    }
+    result
+}
+
+fn letter_counts(word: &str) -> HashMap<char, i32> {
+    let mut result = HashMap::new();
+    for c in word.chars() {
+        let count = result.entry(c).or_insert(0);
+        *count += 1;
+    }
+    result
 }
