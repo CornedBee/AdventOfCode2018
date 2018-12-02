0f30a59ccc4b72fcd137c51b7ae319bba4908984
Sebastian Redl <wasti.redl@gmx.net>
2018-12-02 21:51:39 +0100

    Implement day 1 part 2.

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,6 +1,8 @@
 const INPUT: &str = include_str!("../../inputs/day1");
 
 fn main() -> Result<(), std::num::ParseIntError> {
-    println!("{}", aoc2018::day1(INPUT)?);
+    let results = aoc2018::day1(INPUT)?;
+    println!("{}", results.0);
+    println!("{}", results.1);
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,19 @@
+use std::collections::HashSet;
 use std::str::FromStr;
 
-pub fn day1(input: &str) -> Result<i32, std::num::ParseIntError> {
-    input.lines()
-        .map(<i32 as FromStr>::from_str)
-        .try_fold(0i32, |acc, x| Ok(acc + x?))
+pub fn day1(input: &str) -> Result<(i32, i32), std::num::ParseIntError> {
+    let numbers = input.lines().map(<i32 as FromStr>::from_str).collect::<Result<Vec<_>, _>>()?;
 
+    let part1 = numbers.iter().sum();
+
+    let mut seen = HashSet::new();
+    let mut frequency = 0;
+    for change in numbers.iter().cycle() {
+        frequency += change;
+        if !seen.insert(frequency) {
+            return Ok((part1, frequency));
+        }
+    }
+
+    unreachable!("cycle() is infinite");
 }
