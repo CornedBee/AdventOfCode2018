7d7ffb99272d676227413c30e6fe3691c97526d8
Sebastian Redl <wasti.redl@gmx.net>
2018-12-02 18:47:41 +0100

    Implement day1 part 1.

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,3 +1,6 @@
-fn main() {
-    aoc2018::day1()
+const INPUT: &str = include_str!("../../inputs/day1");
+
+fn main() -> Result<(), std::num::ParseIntError> {
+    println!("{}", aoc2018::day1(INPUT)?);
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
-pub fn day1() {
-    println!("Hello, world!");
+use std::str::FromStr;
+
+pub fn day1(input: &str) -> Result<i32, std::num::ParseIntError> {
+    input.lines()
+        .map(<i32 as FromStr>::from_str)
+        .try_fold(0i32, |acc, x| Ok(acc + x?))
+
 }
