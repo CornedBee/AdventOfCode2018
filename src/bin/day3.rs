b287a0fd0921f740dfcdedd21c2f79146db36c7a
Sebastian Redl <wasti.redl@gmx.net>
2018-12-04 08:59:21 +0100

    Implement day 3 part 1.

---
+++ src/bin/day3.rs
@@ -0,0 +1,10 @@
+use std::num::ParseIntError;
+
+const INPUT: &str = include_str!("../../inputs/day3");
+
+fn main() -> Result<(), ParseIntError> {
+    let results = aoc2018::day3(INPUT)?;
+    println!("{}", results.0);
+    println!("{}", results.1);
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::num::ParseIntError;
 use std::str::FromStr;
+use lazy_static::lazy_static;
+use ndarray::{Array2, s};
+use regex::Regex;
 
-pub fn day1(input: &str) -> Result<(i32, i32), std::num::ParseIntError> {
+pub fn day1(input: &str) -> Result<(i32, i32), ParseIntError> {
     let numbers = input.lines().map(<i32 as FromStr>::from_str).collect::<Result<Vec<_>, _>>()?;
 
     let part1 = numbers.iter().sum();
@@ -70,4 +74,53 @@ fn are_similar_for_similar_strings() {
 
 fn common_subset(s1: &str, s2: &str) -> String {
     s1.chars().zip(s2.chars()).filter(|(c1, c2)| c1 == c2).map(|(c1, _)| c1).collect()
+}
+
+struct Rectangle {
+    left: i32,
+    top: i32,
+    right: i32,
+    bottom: i32,
+}
+
+struct Claim {
+    _id: i32,
+    area: Rectangle,
+}
+
+pub fn day3(input: &str) -> Result<(i32, i32), ParseIntError> {
+    let claims = input.lines().map(parse_claim).collect::<Result<Vec<_>, _>>()?;
+
+    let mut field = Array2::zeros((1000, 1000));
+
+    for claim in &claims {
+        mark_claim(&mut field, claim);
+    }
+
+    let part1 = field.iter().filter(|i| **i > 1).count() as i32;
+
+    Ok((part1, 0))
+}
+
+fn parse_claim(s: &str) -> Result<Claim, ParseIntError> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$").unwrap();
+    }
+    let parsed = RE.captures(s).unwrap();
+    let left = i32::from_str(parsed.get(2).unwrap().as_str())?;
+    let top = i32::from_str(parsed.get(3).unwrap().as_str())?;
+    Ok(Claim {
+        _id: i32::from_str(parsed.get(1).unwrap().as_str())?,
+        area: Rectangle {
+            left,
+            top,
+            right: left + i32::from_str(parsed.get(4).unwrap().as_str())?,
+            bottom: top + i32::from_str(parsed.get(5).unwrap().as_str())?
+        }
+    })
+}
+
+fn mark_claim(field: &mut Array2<i32>, claim: &Claim) {
+    let mut slice = field.slice_mut(s![claim.area.left..claim.area.right, claim.area.top..claim.area.bottom]);
+    slice.map_inplace(|i| *i += 1);
 }
